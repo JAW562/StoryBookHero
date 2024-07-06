dd04af769fb0f428511dcffb70be3afed2f24fa1
JAW562 <jaw9801@gmail.com>
2024-07-06 15:57:21 -0400

    Updated Anims for Tutorial Wizard
    -Added TW NPC
    -Made Animations work

---
+++ Source/StoryBookHero57to77/Private/TutorialWizard.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TutorialWizard.h"
+
+// Sets default values
+ATutorialWizard::ATutorialWizard()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereCol = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCol"));
+
+	SphereCol->SetupAttachment(RootComponent);
+
+	SphereCol->SetGenerateOverlapEvents(true);
+
+	SphereCol->OnComponentBeginOverlap.AddDynamic(this, &ATutorialWizard::OnOverlapBegin);
+
+	SphereCol->OnComponentEndOverlap.AddDynamic(this, &ATutorialWizard::OnOverlapEnd);
+
+
+	SphereCol->SetSphereRadius(30.0f);
+
+	InterTW = CreateDefaultSubobject<UInteractionComponent>(TEXT("InterTW"));
+
+
+	AddOwnedComponent(InterTW);
+
+
+
+
+
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ATutorialWizard::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+void ATutorialWizard::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	UE_LOG(LogTemp, Display, TEXT("OverlapBegin"));
+
+	if (OtherActor->IsA(AScrap::StaticClass()))
+	{
+
+		UE_LOG(LogTemp, Display, TEXT("ActorFound"));
+
+		scrapThere = true;
+
+	}
+
+}
+
+void ATutorialWizard::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
+{
+
+		scrapThere = false;
+
+
+}
+
+void ATutorialWizard::UpdateAnim()
+{
+
+
+	if (scrapThere == true)
+	{
+		UPaperFlipbook* DesiredAnimation = QuestAnim;
+		GetSprite()->SetFlipbook(DesiredAnimation);
+	}
+	else if (scrapThere == false)
+	{
+		UPaperFlipbook* DesiredAnimation = IdleAnim;
+		GetSprite()->SetFlipbook(DesiredAnimation);
+	}
+
+}
+
+// Called every frame
+void ATutorialWizard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateAnim();
+
+
+}
+
---
+++ Source/StoryBookHero57to77/Private/TutorialWizard.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperFlipbookComponent.h"
+#include "InteractionComponent.h"
+#include "Components/SphereComponent.h"
+#include "Scrap.h"
+#include "TutorialWizard.generated.h"
+
+UCLASS()
+class ATutorialWizard : public APaperCharacter
+{
+	GENERATED_BODY()
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = NPCs, meta = (AllowPrivateAccess = true))
+	class USphereComponent* SphereCol;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = NPCs, meta = (AllowPrivateAccess = true))
+	class UInteractionComponent* InterTW;
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = NPCs)
+	class UPaperFlipbook* IdleAnim;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = NPCs)
+	class UPaperFlipbook* QuestAnim;
+
+
+	UFUNCTION(BlueprintCallable, Category = NPCs)
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable, Category = NPCs)
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void UpdateAnim();
+
+
+public:	
+
+	// Sets default values for this actor's properties
+	ATutorialWizard();
+
+	 bool scrapThere;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};
